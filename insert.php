1c3176ebcc9df1202c5d36b234efc9a2a0fba51a
HayashiMebae <me-nyan.ahaha0601@ezweb.ne.jp>
2024-06-03 20:19:39 +0900

    contactform

---
+++ insert.php
@@ -0,0 +1,20 @@
+$pdo->exec("isnert into contactform(name,mail,age,comments)
+values(".$_POST['name']."','"".$_POST['mail']."','".$_POST['age']."','".$_POST['comments']."');");
+<!doctype HTML>
+<html lang="ja">
+
+<head>
+<meta charset="utf-8">
+ <bitle>お問い合わせフォームを作る</title> 
+<link rel="stylesheet"type="text/css"href="style2.css">
+</head>
+
+<body>
+
+  <h1>お問い合わせフォーム</h1>
+
+  <div class="confirm">
+     <p>
+       お問い合わせ有難うございました。<br>3営業日以内に担当者よりご連絡差し上げます。
+     </p>
+ </div>
